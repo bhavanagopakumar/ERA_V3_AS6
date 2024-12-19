ee1d72feb07a6e005959b98e4ab5a88ddc71ef9d
bhavanagopakumar <bhavanag19@gmail.com>
2024-12-19 22:24:07 +0530

    1:2 Added model.py file

---
+++ model.py
@@ -0,0 +1,74 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(1, 32, 3, padding=1),
+            nn.ReLU(),
+            nn.BatchNorm2d(32),
+            nn.Dropout(0.05)
+        )
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(32, 32, 3, padding=1),
+            nn.ReLU(),
+            nn.BatchNorm2d(32),
+            nn.Dropout(0.05)
+        )
+        
+        self.pool1 = nn.Sequential(
+            nn.MaxPool2d(2, 2)
+        )
+
+        self.conv3 = nn.Sequential(
+            nn.Conv2d(32, 16, 3, padding=1),
+            nn.ReLU(),
+            nn.BatchNorm2d(16),
+            nn.Dropout(0.05)
+        )
+
+        self.conv4 = nn.Sequential(
+            nn.Conv2d(16, 16, 3, padding=1),
+            nn.ReLU(),
+            nn.BatchNorm2d(16),
+            nn.Dropout(0.05)
+        )
+        
+        self.pool2 = nn.Sequential(
+            nn.MaxPool2d(2, 2)
+        )
+
+        self.conv5 = nn.Sequential(
+            nn.Conv2d(16, 8, 3),
+            nn.ReLU(),
+            nn.BatchNorm2d(8),
+            nn.Dropout(0.05)
+        )
+
+        self.conv6 = nn.Sequential(
+            nn.Conv2d(8, 8, 3),
+            nn.ReLU(),
+            nn.BatchNorm2d(8),
+            nn.Dropout(0.05)
+        )
+
+        self.conv7 = nn.Sequential(
+            nn.Conv2d(8, 10, 3),
+            nn.ReLU()
+        )
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.pool1(x)
+        x = self.conv3(x)
+        x = self.conv4(x)
+        x = self.pool2(x)
+        x = self.conv5(x)
+        x = self.conv6(x)
+        x = self.conv7(x)
+        x = x.view(-1, 10)
+        return F.log_softmax(x, dim=1) 
